99e41946ec55f0c42eb46c1e9eeffcf3c1475a0a
a-hamzah <Spellbreaker907@gmail.com>
2025-01-15 22:09:59 +0500

    rrt2

---
+++ algorithms/rrt_algo.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+# Settings of python plots
+from matplotlib.pyplot import rcParams
+np.set_printoptions(precision=3, suppress=True)
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = ['Tahoma']
+plt.rcParams['font.size'] = 22
+
+# TreeNode Class
+class treeNode():
+    def __init__(self, locationX, locationY):
+        self.locationX = locationX
+        self.locationY = locationY
+        self.children = []
+        self.parent = None
